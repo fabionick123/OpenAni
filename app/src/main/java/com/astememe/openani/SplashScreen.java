7f26b602893d1f297cb6ed4c220cbbacf0bab6e5
fabionick123 <fabioninahuanca62@gmail.com>
2026-02-10 13:51:00 +0100

    SplashScreen Terminado

--- app/src/main/java/com/astememe/openani/SplashScreen.java
+++ app/src/main/java/com/astememe/openani/SplashScreen.java
@@ -1,6 +1,7 @@
 package com.astememe.openani;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -17,6 +18,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+import java.io.File;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -39,9 +42,9 @@ public class SplashScreen extends AppCompatActivity {
 
         fondo = findViewById(R.id.splashImagenFondo);
         Glide.with(this)
-                .load("https://images.unsplash.com/photo-1609769322709-2de28ae6503a?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+                .load(R.drawable.splashscreen_fondo)
                 .centerCrop()
-                .transition(DrawableTransitionOptions.withCrossFade(3000))
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
                 .into(fondo);
         launchNextActivity();
     }
@@ -53,6 +56,6 @@ public class SplashScreen extends AppCompatActivity {
                 Intent intent = new Intent(SplashScreen.this,MainActivity.class);
                 startActivity(intent);
             }
-        }, 2000);
+        }, 4000);
     }
 }
