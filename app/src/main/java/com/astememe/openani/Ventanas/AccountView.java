8a233a8321572d90e68fb1ba4efc3e411396c9db
StoianAlejandro <s.alejandrostoian@gmail.com>
2026-02-22 15:10:48 +0100

    nose

--- app/src/main/java/com/astememe/openani/Ventanas/AccountView.java
+++ app/src/main/java/com/astememe/openani/Ventanas/AccountView.java
@@ -1,8 +1,12 @@
 package com.astememe.openani.Ventanas;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +17,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.astememe.openani.R;
 
+import java.io.File;
+
+import io.woong.shapedimageview.CircleImageView;
+
 public class AccountView extends AppCompatActivity {
 
 
@@ -20,9 +28,13 @@ public class AccountView extends AppCompatActivity {
     //https://www.reddit.com/r/learnprogramming/comments/q5xelq/should_i_convert_an_image_uri_to_a_blobbytearray/
 
     ConstraintLayout botonAtras;
+    EditText descripcion;
+    CircleImageView fotoPerfil;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_account_view);
@@ -31,7 +43,10 @@ public class AccountView extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        fotoPerfil = findViewById(R.id.fotoperfil);
+        descripcion = findViewById(R.id.descripcion);
         botonAtras = findViewById(R.id.flechaAtrasAcountView);
+        descripcion.setText(preferences.getString("descripcion", "Escriba su descripción aquí"));
         botonAtras.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,5 +54,11 @@ public class AccountView extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        if (preferences.getString("imagen", "").isEmpty()) {
+            fotoPerfil.setImageResource(R.drawable.spiderman);
+        } else {
+            Uri uri = Uri.parse("android.resource://"+this.getPackageName()+"/drawable/foto_de_perfil_" + preferences.getString("imagen", ""));
+            fotoPerfil.setImageURI(uri);
+        }
     }
 }
